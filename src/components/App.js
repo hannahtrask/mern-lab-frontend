a688f6d5bc23e39c801e986ef18678c6136ce49d
Hannah Catherine Trask <hctrask@gmail.com>
2020-10-21 09:19:38 -0600

    styles added and all gardens displayedgit add -A

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,36 @@ import { Route, Link, Switch } from 'react-router-dom';
 import '../scss/app.scss'
 import Form from './Form'
 import Display from './Display'
+import axios from 'axios'
 
 function App() {
+
+
+//url for garden display
+const url = 'https://mern-lab-backend1.herokuapp.com'
+//instantiating instance to set the gardens
+const [gardens, setGardens] = useState([])
+
+//this will get all gardens
+const getGardens = () => {
+	axios.get(url+'/gardens/')
+	.then(res=>{
+		const gardenArr=res.data
+		setGardens(gardenArr)
+	})
+}
+//data received, can see in console log below
+//now pass to display
+console.log(gardens)
+//this will get all gardens on mount
+useEffect(()=>getGardens(), [])
+
+
+
+
+
+
+
   return (
 		<>
 			<nav>
@@ -16,7 +44,11 @@ function App() {
 				<button>ADD A GARDEN</button>
 			</Link>
 			<Switch>
-				<Route exact path='/' component={Display} />
+				<Route exact path='/' 
+					   render={(rp) => <Display {...rp}
+						garden={gardens}
+						/>}
+					/>
 				<Route exact path='/create' component={Form} />
 			</Switch>
 		</>
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,15 +1,43 @@
 import React from 'react'
 import '../scss/app.scss'
+import '../scss/display.scss'
 
 const Display = (props) => {
 
 
-    return (
+    //checking for good prop pass
+    console.log('this is props in display,',props)
+    //this is the garden array
+    const gardenArr = props.garden.data
+    //checking for gardenArr 
+    console.log('this is gardenArr,', gardenArr)
+
+
+    const loaded = () => {
+        if (Array.isArray(gardenArr)) {
+            return (
+            <div className="garden-card">
+                {gardenArr.map((garden) => (
+                    <div className="single-garden">
+                        <img src={garden.image} />
+                        <h2>{garden.name}</h2>
+                        <p>{garden.plants}</p>
+                    </div>
+                    ))}
+            </div>
+
+
+            )
+        }
+    }
+
+
+    const loading = <h2>loading gardens . . .</h2>
+
+return (Array.isArray(gardenArr)) ? loaded() : loading
     
-    <h1 className='display'>display page</h1>
     
     
-    )
 }
 
 export default Display
