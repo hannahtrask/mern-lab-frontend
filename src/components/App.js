2981ee875f5ff7106293725487aa7b99dab24eca
Hannah Catherine Trask <hctrask@gmail.com>
2020-10-21 08:00:16 -0600

    some styles added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
+import '../scss/app.scss'
+import Form from './Form'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hello world</h1>
-      </header>
-    </div>
-  );
+		<>
+			<nav>
+				<Link to='/' className='gardens'>
+					<h1>GARDENS</h1>
+				</Link>
+			</nav>
+			<Link to='/create' className='button'>
+				<button>ADD A GARDEN</button>
+			</Link>
+			<Switch>
+				<Route exact path='/create' component={Form} />
+			</Switch>
+		</>
+	);
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import '../scss/app.scss'
 
 const Form = (props) => {
-    return <h1>this is the form page</h1>
+
+
+//add an edit button here in the display
+
+
+
+
+    return (
+			<div className='form'>
+				<form>
+					<h3>ADD A PLANT TO THE GARDEN</h3>
+					<input type='text' name='plant' value='plant name' />
+
+                    {/*consider making these radio buttons if you have time*/}
+
+					<input type='text' name='edible' value='edible? true or false' />
+					<input type='text' name='vegetable' value='vegetable? t or f' />
+					<input type='text' name='flower' value='edible? t or f' />
+
+
+					<input type='submit' value='GROW' className='submit' />
+				</form>
+			</div>
+		);
+
+
+
+
 }
 
 export default Form
